3b4f170734138ab3a1e65569d50f84a98bf165db
Wagner Morais <wagnermoraiscnt@gmail.com>
2025-08-10 20:02:38 -0300

    feat: create initial socials dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,106 @@
+import Header from "./components/Header";
+import FollowersContainer from "./components/FollowersContainer";
+import OverviewContainer from "./components/OverviewContainer";
+import styles from "./styles/App.module.css";
+
 function App() {
   return (
-    <main>
-      <h1>Hello, world!</h1>
+    <main className={styles.container}>
+      <Header followers={23.004} />
+      <section className={styles.followersSection}>
+        <FollowersContainer
+          social="/images/icon-facebook.svg"
+          username="@nathanf"
+          totalFollowers={1987}
+          audience="Followers"
+          icon="/images/icon-up.svg"
+          dailyChange={12}
+        />
+        <FollowersContainer
+          social="/images/icon-twitter.svg"
+          username="@nathanf"
+          totalFollowers={1044}
+          audience="Followers"
+          icon="/images/icon-up.svg"
+          dailyChange={99}
+        />
+        <FollowersContainer
+          social="/images/icon-instagram.svg"
+          username="@realnathanf"
+          totalFollowers={"11k"}
+          audience="Followers"
+          icon="/images/icon-up.svg"
+          dailyChange={1099}
+        />
+        <FollowersContainer
+          social="/images/icon-youtube.svg"
+          username="Nathan F."
+          totalFollowers={8239}
+          audience="Subscribers"
+          icon="/images/icon-down.svg"
+          dailyChange={144}
+          isAudienceLost
+        />
+      </section>
+      <h2>Overview - Today</h2>
+      <section>
+        <OverviewContainer
+          title="Page Views"
+          social="/images/icon-facebook.svg"
+          dailyTotal={87}
+          icon="/images/icon-up.svg"
+          dailyChange={3}
+        />
+        <OverviewContainer
+          title="Likes"
+          social="/images/icon-facebook.svg"
+          dailyTotal={52}
+          icon="/images/icon-down.svg"
+          dailyChange={2}
+        />
+        <OverviewContainer
+          title="Likes"
+          social="/images/icon-instagram.svg"
+          dailyTotal={5462}
+          icon="/images/icon-up.svg"
+          dailyChange={2257}
+        />
+        <OverviewContainer
+          title="Profile Views"
+          social="/images/icon-instagram.svg"
+          dailyTotal={"52K"}
+          icon="/images/icon-up.svg"
+          dailyChange={1375}
+        />
+        <OverviewContainer
+          title="Retweets"
+          social="/images/icon-twitter.svg"
+          dailyTotal={117}
+          icon="/images/icon-up.svg"
+          dailyChange={303}
+        />
+        <OverviewContainer
+          title="Likes"
+          social="/images/icon-twitter.svg"
+          dailyTotal={507}
+          icon="/images/icon-up.svg"
+          dailyChange={553}
+        />
+        <OverviewContainer
+          title="Likes"
+          social="/images/icon-youtube.svg"
+          dailyTotal={107}
+          icon="/images/icon-down.svg"
+          dailyChange={19}
+        />
+        <OverviewContainer
+          title="Total Views"
+          social="/images/icon-youtube.svg"
+          dailyTotal={1407}
+          icon="/images/icon-up.svg"
+          dailyChange={12}
+        />
+      </section>
     </main>
   );
 }
